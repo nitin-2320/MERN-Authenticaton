61c788bc529edeb47762dfb19a2a5cbd280db89d
Nitin Chaurasiya <168957723+nitin-2320@users.noreply.github.com>
2025-08-26 22:10:20 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {Routes,Route} from 'react-router-dom'
 import EmailVerify from './pages/EmailVerify'
 import ResetPassword from './pages/ResetPassword'
 import Login from './pages/Login'
-import Home from './pages/home'
+import Home from './pages/Home'
 import { ToastContainer} from 'react-toastify';
  import 'react-toastify/dist/ReactToastify.css';
 
